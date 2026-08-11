85a59f21f9385776b024b731f57b93447d06bac0
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 17:14:12 -0400

    Export RIR persistence boundary

--- src/pyxis/rir/__init__.py
+++ src/pyxis/rir/__init__.py
@@ -1,5 +1,11 @@
 """Repository Intermediate Representation for Pyxis."""
 
 from .model import RepositoryIR, WorkspaceIR, build_repository_ir
+from .persistence import persist_repository_ir
 
-__all__ = ["RepositoryIR", "WorkspaceIR", "build_repository_ir"]
+__all__ = [
+    "RepositoryIR",
+    "WorkspaceIR",
+    "build_repository_ir",
+    "persist_repository_ir",
+]
