4043a427590ff85807e3ef4efbb232485fe092ec
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 09:30:47 -0400

    Export normalize_text compiler

--- src/pyxis/compiler/__init__.py
+++ src/pyxis/compiler/__init__.py
@@ -1,3 +1,11 @@
-from .artifacts import GeneratedArtifact, compile_inspect_text
+from .artifacts import (
+    GeneratedArtifact,
+    compile_inspect_text,
+    compile_normalize_text,
+)
 
-__all__ = ["GeneratedArtifact", "compile_inspect_text"]
+__all__ = [
+    "GeneratedArtifact",
+    "compile_inspect_text",
+    "compile_normalize_text",
+]
