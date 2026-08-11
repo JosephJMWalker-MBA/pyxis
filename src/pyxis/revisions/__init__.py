dcc8a6e511f5858d2fe93f5baa7114f15fee021a
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 19:44:04 -0400

    Export revision completion primitives

--- src/pyxis/revisions/__init__.py
+++ src/pyxis/revisions/__init__.py
@@ -1,16 +1,25 @@
 from .model import (
     REVISION_SCHEMA_VERSION,
+    RevisionCompletion,
     RevisionEvent,
     canonical_sha256,
+    create_revision_completion,
     create_revision_event,
 )
-from .persistence import append_revision_event, revision_head_id
+from .persistence import (
+    append_revision_completion,
+    append_revision_event,
+    revision_head_id,
+)
 
 __all__ = [
     "REVISION_SCHEMA_VERSION",
+    "RevisionCompletion",
     "RevisionEvent",
+    "append_revision_completion",
     "append_revision_event",
     "canonical_sha256",
+    "create_revision_completion",
     "create_revision_event",
     "revision_head_id",
 ]
