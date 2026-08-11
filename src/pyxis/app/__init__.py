94ff73f6bcc5f41ffa1f7d3e0ed37e088166b601
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 19:30:34 -0400

    Export preview Apply orchestration

--- src/pyxis/app/__init__.py
+++ src/pyxis/app/__init__.py
@@ -1,3 +1,4 @@
+from .apply import ApplyResult, apply_remove_normalize_text
 from .build import (
     BuildAndRunResult,
     BuildResult,
@@ -11,10 +12,12 @@ from .preview import (
 )
 
 __all__ = [
+    "ApplyResult",
     "ArchitectureDelta",
     "ArchitecturePreview",
     "BuildAndRunResult",
     "BuildResult",
+    "apply_remove_normalize_text",
     "build_and_run_workspace",
     "build_workspace",
     "preview_remove_normalize_text",
