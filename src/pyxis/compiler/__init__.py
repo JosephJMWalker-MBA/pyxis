e4922ad109d2a3bb5139a7bb5e35a858c0ca612d
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 17:20:29 -0400

    Export generation manifest APIs

--- src/pyxis/compiler/__init__.py
+++ src/pyxis/compiler/__init__.py
@@ -4,14 +4,24 @@ from .artifacts import (
     compile_normalize_text,
     compile_workspace_entrypoint,
 )
+from .manifest import (
+    GenerationManifest,
+    ManifestArtifact,
+    build_generation_manifest,
+    persist_generation_manifest,
+)
 from .materialize import materialize_artifacts
 from .repository import compile_repository
 
 __all__ = [
     "GeneratedArtifact",
+    "GenerationManifest",
+    "ManifestArtifact",
+    "build_generation_manifest",
     "compile_inspect_text",
     "compile_normalize_text",
     "compile_repository",
     "compile_workspace_entrypoint",
     "materialize_artifacts",
+    "persist_generation_manifest",
 ]
