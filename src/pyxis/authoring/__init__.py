15ed4d0c436652d3a382154ab7a2587b8d92ba4f
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 17:08:53 -0400

    Export canonical Workspace persistence

--- src/pyxis/authoring/__init__.py
+++ src/pyxis/authoring/__init__.py
@@ -1,3 +1,8 @@
+from .persistence import persist_workspace_spec
 from .workspace import WorkspaceSpec, create_workspace_spec
 
-__all__ = ["WorkspaceSpec", "create_workspace_spec"]
+__all__ = [
+    "WorkspaceSpec",
+    "create_workspace_spec",
+    "persist_workspace_spec",
+]
