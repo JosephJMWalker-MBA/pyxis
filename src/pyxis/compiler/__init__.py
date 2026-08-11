75d27924521d390a93049915d00e248f58b04dbf
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 09:37:25 -0400

    Export repository compiler

--- src/pyxis/compiler/__init__.py
+++ src/pyxis/compiler/__init__.py
@@ -4,10 +4,12 @@ from .artifacts import (
     compile_normalize_text,
     compile_workspace_entrypoint,
 )
+from .repository import compile_repository
 
 __all__ = [
     "GeneratedArtifact",
     "compile_inspect_text",
     "compile_normalize_text",
+    "compile_repository",
     "compile_workspace_entrypoint",
 ]
