8beabbabff0f4f40bcdb21450c481cdd6cc64a65
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 09:33:02 -0400

    Export Workspace composition compiler

--- src/pyxis/compiler/__init__.py
+++ src/pyxis/compiler/__init__.py
@@ -2,10 +2,12 @@ from .artifacts import (
     GeneratedArtifact,
     compile_inspect_text,
     compile_normalize_text,
+    compile_workspace_entrypoint,
 )
 
 __all__ = [
     "GeneratedArtifact",
     "compile_inspect_text",
     "compile_normalize_text",
+    "compile_workspace_entrypoint",
 ]
