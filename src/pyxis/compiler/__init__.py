aaf70891f19d00f041073bf9477142822c6fde1d
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 19:13:23 -0400

    Export manifest reconciliation boundaries

--- src/pyxis/compiler/__init__.py
+++ src/pyxis/compiler/__init__.py
@@ -8,20 +8,28 @@ from .manifest import (
     GenerationManifest,
     ManifestArtifact,
     build_generation_manifest,
+    load_generation_manifest,
     persist_generation_manifest,
 )
-from .materialize import materialize_artifacts
+from .materialize import (
+    MaterializationResult,
+    materialize_artifacts,
+    reconcile_materialized_artifacts,
+)
 from .repository import compile_repository
 
 __all__ = [
     "GeneratedArtifact",
     "GenerationManifest",
     "ManifestArtifact",
+    "MaterializationResult",
     "build_generation_manifest",
     "compile_inspect_text",
     "compile_normalize_text",
     "compile_repository",
     "compile_workspace_entrypoint",
+    "load_generation_manifest",
     "materialize_artifacts",
     "persist_generation_manifest",
+    "reconcile_materialized_artifacts",
 ]
