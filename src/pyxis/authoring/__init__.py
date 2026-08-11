9aa3fdbc6b7bb06271341cb4ec52c30a5819acce
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 19:29:54 -0400

    Export canonical Workspace loader

--- src/pyxis/authoring/__init__.py
+++ src/pyxis/authoring/__init__.py
@@ -1,8 +1,9 @@
-from .persistence import persist_workspace_spec
+from .persistence import load_workspace_spec, persist_workspace_spec
 from .workspace import WorkspaceSpec, create_workspace_spec
 
 __all__ = [
     "WorkspaceSpec",
     "create_workspace_spec",
+    "load_workspace_spec",
     "persist_workspace_spec",
 ]
