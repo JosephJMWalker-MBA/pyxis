687e2dafa0281f00f49580420b9176a78154e411
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 18:31:36 -0400

    Export architecture preview API

--- src/pyxis/app/__init__.py
+++ src/pyxis/app/__init__.py
@@ -4,10 +4,18 @@ from .build import (
     build_and_run_workspace,
     build_workspace,
 )
+from .preview import (
+    ArchitectureDelta,
+    ArchitecturePreview,
+    preview_remove_normalize_text,
+)
 
 __all__ = [
+    "ArchitectureDelta",
+    "ArchitecturePreview",
     "BuildAndRunResult",
     "BuildResult",
     "build_and_run_workspace",
     "build_workspace",
+    "preview_remove_normalize_text",
 ]
