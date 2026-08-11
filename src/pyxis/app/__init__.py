693e24b9c553cd0f7717d852dbab608992dcbd78
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 10:39:44 -0400

    Export build-and-run orchestration

--- src/pyxis/app/__init__.py
+++ src/pyxis/app/__init__.py
@@ -1,3 +1,13 @@
-from .build import BuildResult, build_workspace
+from .build import (
+    BuildAndRunResult,
+    BuildResult,
+    build_and_run_workspace,
+    build_workspace,
+)
 
-__all__ = ["BuildResult", "build_workspace"]
+__all__ = [
+    "BuildAndRunResult",
+    "BuildResult",
+    "build_and_run_workspace",
+    "build_workspace",
+]
