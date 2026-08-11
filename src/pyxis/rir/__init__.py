586c1ab1b764fd6cfdee05e63c3b70f0b3e36983
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 09:17:32 -0400

    Add RIR package boundary

---
+++ src/pyxis/rir/__init__.py
@@ -0,0 +1,5 @@
+"""Repository Intermediate Representation for Pyxis."""
+
+from .model import RepositoryIR, WorkspaceIR, build_repository_ir
+
+__all__ = ["RepositoryIR", "WorkspaceIR", "build_repository_ir"]
