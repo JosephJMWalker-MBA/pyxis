8eb1c46e9a7b16ab487ea744b41292ceb7bb285c
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 22:12:37 -0400

    Export materialization API

--- src/pyxis/exporting/__init__.py
+++ src/pyxis/exporting/__init__.py
@@ -1,7 +1,10 @@
+from .materialize import ExportMaterializationResult, materialize_export_plan
 from .plan import ExportCompilerProduct, ExportPlan, build_export_plan
 
 __all__ = [
     "ExportCompilerProduct",
+    "ExportMaterializationResult",
     "ExportPlan",
     "build_export_plan",
+    "materialize_export_plan",
 ]
