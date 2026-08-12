6fa0fdeefb530651e33a612831c0a27dcaaad3b2
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 22:18:08 -0400

    Export Repository RIR loader

--- src/pyxis/rir/__init__.py
+++ src/pyxis/rir/__init__.py
@@ -1,11 +1,12 @@
 """Repository Intermediate Representation for Pyxis."""
 
 from .model import RepositoryIR, WorkspaceIR, build_repository_ir
-from .persistence import persist_repository_ir
+from .persistence import load_repository_ir, persist_repository_ir
 
 __all__ = [
     "RepositoryIR",
     "WorkspaceIR",
     "build_repository_ir",
+    "load_repository_ir",
     "persist_repository_ir",
 ]
