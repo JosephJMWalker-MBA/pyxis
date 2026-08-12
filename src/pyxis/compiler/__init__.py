294fd5b71f15cc3b513d141787afcfe5734d8d98
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 21:15:03 -0400

    Export generation status evidence APIs

--- src/pyxis/compiler/__init__.py
+++ src/pyxis/compiler/__init__.py
@@ -14,22 +14,34 @@ from .manifest import (
 )
 from .materialize import (
     MaterializationResult,
+    inspect_materialized_artifact_integrity,
     materialize_artifacts,
     reconcile_materialized_artifacts,
 )
 from .repository import compile_repository
+from .status import (
+    ArtifactGenerationStatus,
+    ExistingArtifactIntegrity,
+    GenerationStatus,
+    classify_generation_statuses,
+)
 
 __all__ = [
+    "ArtifactGenerationStatus",
+    "ExistingArtifactIntegrity",
     "GeneratedArtifact",
     "GenerationManifest",
+    "GenerationStatus",
     "ManifestArtifact",
     "MaterializationResult",
     "build_generation_manifest",
+    "classify_generation_statuses",
     "compile_inspect_text",
     "compile_normalize_text",
     "compile_repository",
     "compile_workspace_entrypoint",
     "generation_manifest_sha256",
+    "inspect_materialized_artifact_integrity",
     "load_generation_manifest",
     "materialize_artifacts",
     "persist_generation_manifest",
