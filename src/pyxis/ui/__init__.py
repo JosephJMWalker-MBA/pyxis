c3158c9f79e62ed13058dcb9eb3d24966ef5d928
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-12 17:37:21 -0400

    Export architecture preview renderer

--- src/pyxis/ui/__init__.py
+++ src/pyxis/ui/__init__.py
@@ -1,6 +1,12 @@
-from .textual_shell import WorkspaceDetail, WorkspaceShell, create_workspace_shell
+from .textual_shell import (
+    ArchitecturePreviewDetail,
+    WorkspaceDetail,
+    WorkspaceShell,
+    create_workspace_shell,
+)
 
 __all__ = [
+    "ArchitecturePreviewDetail",
     "WorkspaceDetail",
     "WorkspaceShell",
     "create_workspace_shell",
