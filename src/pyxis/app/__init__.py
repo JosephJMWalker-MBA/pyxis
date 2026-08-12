8517384a2472a2072186d646b7647c5255bcd4d9
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-12 17:15:08 -0400

    Export architecture preview application seam

--- src/pyxis/app/__init__.py
+++ src/pyxis/app/__init__.py
@@ -3,13 +3,20 @@ from .apply import (
     apply_remove_normalize_text,
     apply_restore_normalize_text,
 )
+from .architecture_preview import (
+    WorkspaceArchitecturePreviewResult,
+    preview_workspace_remove_normalize_text,
+)
 from .build import (
     BuildAndRunResult,
     BuildResult,
     build_and_run_workspace,
     build_workspace,
 )
-from .controller import WorkspaceRuntimeController
+from .controller import (
+    WorkspaceArchitecturePreviewController,
+    WorkspaceRuntimeController,
+)
 from .export import WorkspaceExportResult, export_workspace
 from .operations import WorkspaceRerunResult, rerun_workspace
 from .presentation import (
@@ -27,19 +34,28 @@ from .preview import (
     preview_remove_normalize_text,
     preview_restore_normalize_text,
 )
+from .preview_presentation import (
+    ArchitecturePreviewPresentation,
+    CanonicalPreviewPresentation,
+    create_architecture_preview_presentation,
+)
 from .query import query_workspace_presentation
 
 __all__ = [
     "ApplyResult",
     "ArchitectureDelta",
     "ArchitecturePreview",
+    "ArchitecturePreviewPresentation",
     "BuildAndRunResult",
     "BuildResult",
     "CanonicalPresentation",
+    "CanonicalPreviewPresentation",
     "CompilerArtifactPresentation",
     "ExportPresentation",
     "RIRPresentation",
     "RevisionPresentation",
+    "WorkspaceArchitecturePreviewController",
+    "WorkspaceArchitecturePreviewResult",
     "WorkspaceExportResult",
     "WorkspacePresentation",
     "WorkspaceRerunResult",
@@ -48,10 +64,12 @@ __all__ = [
     "apply_restore_normalize_text",
     "build_and_run_workspace",
     "build_workspace",
+    "create_architecture_preview_presentation",
     "create_workspace_presentation",
     "export_workspace",
     "preview_remove_normalize_text",
     "preview_restore_normalize_text",
+    "preview_workspace_remove_normalize_text",
     "query_workspace_presentation",
     "rerun_workspace",
 ]
