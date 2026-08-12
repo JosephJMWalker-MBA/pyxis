3ab3f115dff3cf4138ad3db153f29e2cc04e6e43
JosephJMWalker-MBA <joseph@decisionpointinitiative.com>
2026-08-11 22:19:07 -0400

    Export identity verification API

--- src/pyxis/exporting/__init__.py
+++ src/pyxis/exporting/__init__.py
@@ -1,10 +1,18 @@
 from .materialize import ExportMaterializationResult, materialize_export_plan
 from .plan import ExportCompilerProduct, ExportPlan, build_export_plan
+from .verify import (
+    ExportIdentityVerificationResult,
+    VerifiedExportCompilerProduct,
+    verify_export_identity,
+)
 
 __all__ = [
     "ExportCompilerProduct",
+    "ExportIdentityVerificationResult",
     "ExportMaterializationResult",
     "ExportPlan",
+    "VerifiedExportCompilerProduct",
     "build_export_plan",
     "materialize_export_plan",
+    "verify_export_identity",
 ]
